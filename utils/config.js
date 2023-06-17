4766ee7690bf3cf2079b30ae0a528f4d00a649f0
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-06-17 16:42:03 +0800

    added functionalities

--- utils/config.js
+++ utils/config.js
@@ -3,8 +3,10 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const SECRET = process.env.SECRET;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export default {
   PORT,
   SECRET,
+  MONGODB_URI,
 };
